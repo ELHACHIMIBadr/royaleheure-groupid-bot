ff71398ed97788564398061032c59b6a69a28def
ELHACHIMIBadr <badr.elhachimi1998@gmail.com>
2025-07-03 19:44:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-# On récupère les variables d'environnement
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID")
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
-    if chat.type in ['group', 'supergroup']:
+    if chat.type in ["group", "supergroup"]:
         await update.message.reply_text(f"Group ID: {chat.id}")
     else:
-        await update.message.reply_text("Merci de m’ajouter dans un groupe pour détecter son ID.")
+        await update.message.reply_text("Ajoute-moi dans un groupe pour afficher son ID.")
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
